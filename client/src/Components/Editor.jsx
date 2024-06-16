ed1f105e35514413728c30ac45d8f0319b0e22ae
Ramcharan <ramcharan2k4@gmail.com>
2024-06-16 12:11:45 +0530

    Build

--- client/src/Components/Editor.jsx
+++ client/src/Components/Editor.jsx
@@ -50,8 +50,8 @@ const Editor = () => {
   }, []);
 
   useEffect(() => {
-    // const socketServer = io("https://writewaveserver.onrender.com");
-    const socketServer = io("http://localhost:9000");
+    const socketServer = io("https://writewaveserver.onrender.com");
+    // const socketServer = io("http://localhost:9000");
     setSocket(socketServer);
 
     return () => {
